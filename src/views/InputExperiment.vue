4c299aff3344e90345062bd9c16777dce58cfc2c
Mariana <mariana.bocoi@gmail.com>
2018-11-24 14:48:45 +0100

    initial tilt

--- src/views/InputExperiment.vue
+++ src/views/InputExperiment.vue
@@ -1,15 +1,40 @@
 <template>
-  <h1>Hello {{angle}}!</h1>
+  <h1>Unicorns! <br/>{{y}}g</h1>
 </template>
 
 <script>
+
 export default {
   name: 'inputexperiment',
   mounted () {
+    movementListener(([xx, yy, zz]) => {
+      this.x = Math.round(xx)
+      this.y = Math.round(yy)
+      this.z = Math.round(zz)
+    })
     this.angle = 10
   },
   data: () => ({
-    angle: 0
+    angle: 0,
+    x: 0,
+    y: 0,
+    z: 0
   })
+}
+
+const movementListener = (tilt) => {
+  if (window.DeviceOrientationEvent) {
+    window.addEventListener('deviceorientation', function (event) {
+      tilt([event.beta, event.gamma, event.alpha])
+    }, true)
+  } else if (window.DeviceMotionEvent) {
+    window.addEventListener('devicemotion', function (event) {
+      tilt([event.acceleration.x * 2, event.acceleration.y * 2, event.acceleration.z])
+    }, true)
+  } else {
+    window.addEventListener('MozOrientation', function (event) {
+      tilt([event.orientation.x * 50, event.orientation.y * 50, event.orientation * 50])
+    }, true)
+  }
 }
 </script>
