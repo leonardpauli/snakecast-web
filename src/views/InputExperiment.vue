cd4846464500a503931f4dc8d5f227be8c889e2c
Mariana <mariana.bocoi@gmail.com>
2018-11-24 16:41:32 +0100

    websocket client initial

--- src/views/InputExperiment.vue
+++ src/views/InputExperiment.vue
@@ -2,7 +2,7 @@
 h1
 	| Unicorns!
 	br
-	| {{ y }}g
+	| {{ y }}hh
 </template>
 
 <script>
@@ -14,17 +14,34 @@ export default {
 		x: 0,
 		y: 0,
 		z: 0,
+	  connection: new WebSocket('ws://localhost:9030'),
 	}),
 	mounted () {
 		movementListener(([xx, yy, zz])=> {
 			this.x = Math.round(xx)
 			this.y = Math.round(yy)
 			this.z = Math.round(zz)
+			this.connection.send(yy)
 		})
+		mouseListener(([xx, yy, zz])=> {
+			this.x = Math.round(xx)
+			this.y = Math.round(yy)
+			this.z = Math.round(zz)
+			this.connection.send(yy)
+		})
+		// Log errors
+		this.connection.onerror = error=> {
+			console.error('WebSocket Error ' + error)
+		}
 		this.angle = 10
 	},
 }
 
+const mouseListener = funny=> {
+	window.addEventListener('mousemove', event=> {
+		funny([event.x, event.y, 42])
+	}, true)
+}
 const movementListener = tilt=> {
 	if (window.DeviceOrientationEvent) {
 		window.addEventListener('deviceorientation', event=> {
