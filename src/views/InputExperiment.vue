5716eb464df075b58024eeb393e83f64d5d38d4b
Leonard Pauli <leonardpauli@me.com>
2018-11-24 13:50:38 +0100

    page: input-experiment empty added

---
+++ src/views/InputExperiment.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Hello {{angle}}!</h1>
+</template>
+
+<script>
+export default {
+  name: 'inputexperiment',
+  mounted () {
+    this.angle = 10
+  },
+  data: () => ({
+    angle: 0
+  })
+}
+</script>
